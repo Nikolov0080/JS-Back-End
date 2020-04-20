4cbec9f8ccff77edb88b78da507da3407c3b30e3
Zhivko Nikolov <49612284+Nikolov0080@users.noreply.github.com>
2020-04-20 19:18:30 +0300

    Add files via upload

---
+++ 03. Cubicle-Workshop-Part-1-Resources/config/routes.js
@@ -0,0 +1,13 @@
+// TODO: Require Controllers...
+const cubeController = require('../controllers/cube');
+
+
+module.exports = (app) => {
+
+    app.get('/details/:id', cubeController.details)
+    app.get('/create', cubeController.getCreate)
+    app.post('/create', cubeController.postCreate)
+    app.get('/about', cubeController.about)
+    app.get('/not-found', cubeController.notFound)
+    app.get('/', cubeController.index)
+};
