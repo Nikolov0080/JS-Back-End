ab97a3b6c27542f21d691fbafccda041a326a7ac
Nikolov0080 <apofis12343@gmail.com>
2020-05-03 12:04:16 +0300

    database create method added (mongodb)

---
+++ Self-Projects/ExpressJS_Self-Project_1/myapp/DataBase/db.js
@@ -0,0 +1,20 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    createItem(db)
+});
+
+function createItem(db) {
+    var dbo = db.db("ExpressAppDB");
+    exports.create = function (email) {
+        dbo.collection("emails").insertOne({email}, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    }
+
+}
+
--- Self-Projects/ExpressJS_Self-Project_1/myapp/controllers/landing.js
+++ Self-Projects/ExpressJS_Self-Project_1/myapp/controllers/landing.js
@@ -1,3 +1,6 @@
+let dataBase = require('../DataBase/db');
+
+
 exports.get_landing = function (req, res, next) {
     res.render('landing', { title: 'Express' });
 }
@@ -5,5 +8,6 @@ exports.get_landing = function (req, res, next) {
 
 exports.submit_lead = function (req, res, next) {
     console.log(req.body);
+    dataBase.create(req.body.lead_email);
     res.redirect('/')
 }
