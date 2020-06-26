08439d21e22a94e2dc256f982ab594233d093f47
Nikolov0080 <apofis12343@gmail.com>
2020-06-26 13:13:29 +0300

    feat: isAuth function description added

--- backendExamsEX/Exam_Skeleton/utils/jwt.js
+++ backendExamsEX/Exam_Skeleton/utils/jwt.js
@@ -3,10 +3,10 @@ const { secret } = require('../config/config');
 
 
 module.exports = {
-    createToken(data) {
+    createToken(data) {// creates the jwt so it can be used
         return jwt.sign({ _id: data._id }, secret, { expiresIn: '6h' });
     },
-    verifyToken(token) {
+    verifyToken(token) { // checks for a token and returns promise or rejection
         return new Promise((resolve, reject) => {
             jwt.verify(token, secret, (err, data) => {
                 if (err) { reject(err); return; }
